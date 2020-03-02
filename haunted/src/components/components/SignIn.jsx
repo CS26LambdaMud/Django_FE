a8427583d985cb3a512869d3a5c2dd66388685c3
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-03-02 13:51:15 -0600

    added basics removed duplicates

---
+++ haunted/src/components/components/SignIn.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import axios from 'axios';
+
+class SignIn extends React.Component {
+    constructor(props){
+        super(props)
+
+    this.state={ 
+        credentials:{
+            username: '',
+            password: ''
+        },
+        error: ''
+        }
+    };
+    handleChange = e => {
+        this.setState({
+            credentials:{
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+    handleSubmit = e => {
+        // let localStorage;
+        e.preventDefault();
+        axios.post('api/login', this.state.credentials)
+        .then(res => {
+            console.log('Res', res.data.key)
+           localStorage.setItem('token', res.data.key)
+            console.log('Signed in Successful')
+            this.props.history.push('/play');
+        })
+        .catch(err => {
+            this.setState({
+                ...this.state,
+                error: 'Incorrect email or password'
+            })
+            console.log(err)
+        })
+    }
+    render(){
+        return(
+            <div className='main-container'>
+                <img className='bg-img' src='./mainbg.jpeg'/>
+                <div className='signin'>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                    type="text"
+                    name="username"
+                    placeholder="username"
+                    value={this.state.credentials.username}
+                    onChange={this.handleChange}
+                    required/>
+                    <br/>
+                    <input
+                    type="password"
+                    name="password"
+                    value={this.state.credentials.password}
+                    onChange={this.handleChange}
+                    placeholder="password"
+                    required/>
+                    <button>Login</button>
+                    <p className='login-error'>
+                        {this.state.error ? this.state.error : null}
+                    </p>
+                </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
---
+++ haunted/src/utils/AxiosWithAuth.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem("token");
+
+    return axios.create({
+        baseURL: 'deployed link',
+        headers: {
+            // Authorization: "Token fd80ff4bdfaced495a7991320427a82b98b3a98b"
+            Authorization: `Token ${token}`
+        }
+    });
+};
+
+export default axiosWithAuth;
