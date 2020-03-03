0a6b2b506059e124f813c4ffd1e1a8c598992525
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-03-03 15:28:17 -0600

    Update Map.js

--- haunted/src/components/map/Map.js
+++ haunted/src/components/map/Map.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import './Map.css'
 
 class Map extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      square : {
+        x: 0,
+        y: 0
+      }
+    }
+  }
+
+
 
   render(){
     return(
