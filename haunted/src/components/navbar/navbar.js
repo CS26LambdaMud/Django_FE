25d91c5e6ae7cdc727a306275785ec879d1347ad
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-03-03 10:53:32 -0800

    made a basic navigation bar

---
+++ haunted/src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import {
+    container
+} from "./navbar.module.css"
+
+const Navbar = () => {
+    return (
+      <div className={container}>
+          <Link to="/">Home</Link>
+          <Link to="/play">Play</Link>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+      </div>  
+    );
+}
+
+export default Navbar
