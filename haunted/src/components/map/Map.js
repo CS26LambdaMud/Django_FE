8e87c947e75f2eec7a2fd949238e8156b07ebb57
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-03-04 12:16:15 -0600

    Map Styling
    The map canvas has been fixed to fit all screens, working on button to display data once it's clicked on. Might change it though

--- haunted/src/components/map/Map.js
+++ haunted/src/components/map/Map.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
 import './Map.css'
+import axios from 'axios'
 
 class Map extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      room: {
+          x: '',
+          y: ''
+        }
+      }
+    }
+
+    map_obj = () => {
+      console.log('button pressed')
+
+    }
 
   render(){
     return(
-      <div class='map'>
-        <canvas ref='canvas' width={ 640 } height={ 425 } />
+      <div>
+      <h1> Haunted </h1>
+        <canvas className='map' ref='canvas' width={ 720 } height={ 450 }> </canvas>
+
+        <br/>
+
+        <button
+          type="button"
+          onClick={this.map_obj}
+          >
+          Start Game
+        </button>
       </div>
     )
   }
