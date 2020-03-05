a074864252da4f6eb5d26a557bd88c7249145aca
josemariodev <josemariodev@gmail.com>
2020-03-05 16:39:54 -0600

    pull from master

--- haunted/src/components/Game/Game.js
+++ haunted/src/components/Game/Game.js
@@ -24,7 +24,7 @@ const Game = () => {
     return <>
 
       <canvas width={mapSize * roomSize} height={mapSize * roomSize} ref={canvasRef}/>
-      <Map/>      
+      <Map/>
       <DirectionPad/>
     </>;
 
