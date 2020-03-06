bf1680d8f4f5212cbb2177f68d47a5de717fc652
josemariodev <josemariodev@gmail.com>
2020-03-06 10:47:33 -0600

    Navbar styling, add logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,22 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import logo from './haunted.png'
 import {
     container
 } from "./navbar.module.css"
 
 const Navbar = () => {
     return (
+        <div className="top-container">
       <div className={container}>
+          <img src={logo} alt="logo"/>
           <Link to="/">Home</Link>
           <Link to="/play">Play</Link>
           <Link to="/signin">Sign In</Link>
           <Link to="/signup">Sign Up</Link>
       </div>
+        </div>
+    
     );
 }
 
